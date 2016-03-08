11e6c014160d5852755ccece46490ee74f3a24b2
Phelipe <phelipewesleydeoliveira@gmail.com>
2016-03-08 15:46:49 -0300

    Adicionado plugin inappbrowser do cordova

--- www/js/controllers/loginController.js
+++ www/js/controllers/loginController.js
@@ -12,7 +12,7 @@ Oauth.url="http://52.20.21.167:3500/oauth/";
 Oauth.client_id= "5f7396418354799903da39cfbf7dbe11";
 Oauth.client_secret= "9a41e2c35f4a143f7aae1d96b8cb8e18";
 
-var url = Oauth.url+'new?client_id='+ Oauth.client_id + '&client_secret='+ Oauth.client_secret +'&redirect_uri=http://localhost:8100' ;
+var url = Oauth.url+'new?client_id='+ Oauth.client_id + '&client_secret='+ Oauth.client_secret +'&redirect_uri=http://localhost' ;
 //url="http://52.20.21.167:3500/oauth/token?client_id=5f7396418354799903da39cfbf7dbe11&client_secret=9a41e2c35f4a143f7aae1d96b8cb8e18&email=operador@smartq.com.br&password=smartquadro";
  // Open in app browser
  window.open(url,'_blank');

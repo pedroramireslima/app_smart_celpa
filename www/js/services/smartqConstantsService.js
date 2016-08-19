7cbf1bcb84e74e82efde64198b9ce75f7a02a398
Phelipe <phelipewesleydeoliveira@gmail.com>
2016-08-19 16:44:53 -0300

    Correções do app

--- www/js/services/smartqConstantsService.js
+++ www/js/services/smartqConstantsService.js
@@ -4,8 +4,11 @@ angular.module('smartq').constant('config',{
 
 //Constantes de acesso ao servidor
 SERVER:{
+     //url:"http://52.20.21.167",
      url:"http://dev.smartquadro.com.br",
+     //port:"80",
      port:"3600",
+     //token:"3fecec9ba2b7508738e066a000d0f104"
      token:"e0c392bbd35e1a18710fb6a00483909a"
 }
 

80015df80cb5bac60b1155b03b072840a9b48824
Phelipe Wesley <phelipewesleydeoliveira@gmail.com>
2016-09-26 13:51:32 -0300

    Revert "deletado brannch production"
    This reverts commit 75c455b4d6439cb6457fa0217b3063aaaa339249.

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,11 @@ var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var sh = require('shelljs');
 
-var templateCache = require('gulp-angular-templatecache');
-var ngAnnotate = require('gulp-ng-annotate');
-var useref = require('gulp-useref');
-
 var paths = {
-  sass: ['./scss/**/*.scss'],
-  templateCache: ['./www/templates/**/*.html'],
-  ng_annotate: ['./www/js/**/*.js'],
-  useref: ['./www/*.html']
+  sass: ['./scss/**/*.scss']
 };
 
-gulp.task('default', ['sass', 'templatecache', 'ng_annotate', 'useref']);
+gulp.task('default', ['sass']);
 
 gulp.task('sass', function(done) {
   gulp.src('./scss/ionic.app.scss')
@@ -35,11 +28,6 @@ gulp.task('sass', function(done) {
 
 gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.templatecache, ['templatecache']);
-  gulp.watch(paths.ng_annotate, ['ng_annotate']);
-  gulp.watch(paths.useref, ['useref']);
-
-
 });
 
 gulp.task('install', ['git-check'], function() {
@@ -61,30 +49,3 @@ gulp.task('git-check', function(done) {
   }
   done();
 });
-
-gulp.task('templatecache', function(done){
-  gulp.src('./www/templates/**/*.html')
-  .pipe(templateCache({standalone:true}))
-  .pipe(gulp.dest('./www/js'))
-  .on('end', done);
-});
-
-gulp.task('ng_annotate', function (done) {
-  gulp.src('./www/js/**/*.js')
-  .pipe(ngAnnotate({single_quotes: true}))
-  .pipe(gulp.dest('./www/dist/dist_js/app'))
-  .on('end', done);
-});
-
-gulp.task('useref', function (done) {
-  var assets = useref.assets();
-  gulp.src('./www/*.html')
-  .pipe(assets)
-  .pipe(assets.restore())
-  .pipe(useref())
-  .pipe(gulp.dest('./www/dist'))
-  .on('end', done);
-});
-
-
-
